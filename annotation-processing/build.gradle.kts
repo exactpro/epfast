e56f3beb073e39765a2a4a279fc5f202d221fc1d
lashamez <lashakapanadze50@gmail.com>
2019-09-23 14:28:08 +0400

    switch to JUnit 5; add checkstyle

--- annotation-processing/build.gradle.kts
+++ annotation-processing/build.gradle.kts
@@ -1,3 +1,5 @@
+val junitVersion: String by rootProject.extra
+
 plugins {
     `java-library`
 }
@@ -7,9 +9,8 @@ repositories {
 }
 
 dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     annotationProcessor("com.google.auto.service:auto-service:1.0-rc6")
-
     compileOnly("com.google.auto.service:auto-service:1.0-rc6")
-
-    testImplementation("junit:junit:4.12")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
+val junitVersion by extra { "5.5.2" }
 plugins {
     java
+    checkstyle
 }
 
 group = "com.exactpro"
@@ -9,10 +11,18 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testCompile("junit", "junit", "4.12")
+allprojects{
+    apply(plugin="java")
+    apply(plugin="checkstyle")
+
+    checkstyle {
+        toolVersion="7.8.1"
+        configFile=rootProject.file("config/checkstyle/checkstyle.xml")
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
 }
 
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
