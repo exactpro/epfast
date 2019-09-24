a77da39894a628e42db5eef36d35a978ac429a6e
Anton Sitnikov <anton.sitnikov@exactprosystems.com>
2019-09-24 13:55:16 +0400

    build files refactoring

--- annotation-processing/build.gradle.kts
+++ annotation-processing/build.gradle.kts
@@ -4,13 +4,9 @@ plugins {
     `java-library`
 }
 
-repositories {
-    jcenter()
-}
-
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
     annotationProcessor("com.google.auto.service:auto-service:1.0-rc6")
     compileOnly("com.google.auto.service:auto-service:1.0-rc6")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
 }
