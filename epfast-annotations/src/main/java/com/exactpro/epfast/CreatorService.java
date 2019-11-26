676f17a8d64cc138f6a31dec936b66e31db4b672
lashamez <lashakapanadze50@gmail.com>
2019-11-26 15:42:49 +0400

    Service class lazy initialization

--- epfast-annotations/src/main/java/com/exactpro/epfast/CreatorService.java
+++ epfast-annotations/src/main/java/com/exactpro/epfast/CreatorService.java
@@ -7,13 +7,20 @@ import java.util.stream.Collectors;
 import static com.exactpro.epfast.util.Stream.streamIterable;
 
 public class CreatorService {
-    private static Collection<ICreator> providers = getProviders();
+    private static Collection<ICreator> providers;
 
     private static Collection<ICreator> getProviders() {
         return streamIterable(ServiceLoader.load(ICreator.class)).collect(Collectors.toList());
     }
 
     public static ICreator getCreator(String fastPackage) {
+        if (providers == null) {
+            synchronized (CreatorService.class) {
+                if (providers == null) {
+                    providers = getProviders();
+                }
+            }
+        }
         return providers.stream().filter(iCreator -> iCreator.getFastPackage().equals(fastPackage))
             .findFirst().orElseThrow(() ->
                 new RuntimeException(String.format("Creator with FastPackage=\"%s\" not found", fastPackage)));
