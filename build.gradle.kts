32e4c8eea57967f2ebfe01fb8037b9ce39ac6a2e
Anton Sitnikov <anton.sitnikov@exactprosystems.com>
2020-03-04 14:40:27 +0400

    Add kotlin support; Gradle build cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,17 @@
-val junitVersion by extra { "5.5.2" }
-val assertjVersion by extra { "3.14.0" }
+val assertjVersion by extra { "3.15.0" }
+val junitVersion by extra { "5.6.0" }
+val nettyVersion by extra { "4.1.45.Final" }
 
 plugins {
-    java
+    kotlin("jvm") version "1.3.61"
+    `java-library`
     checkstyle
 }
 
 group = "com.exactpro"
 version = "0.1-SNAPSHOT"
 
-allprojects{
+allprojects {
     apply(plugin="java")
     apply(plugin="checkstyle")
 
@@ -17,18 +19,18 @@ allprojects{
         mavenCentral()
     }
 
-    configure<JavaPluginConvention> {
+    java {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
-    }
-
-    tasks.named<Test>("test") {
-        useJUnitPlatform()
     }
 
     checkstyle {
         toolVersion = "8.26"
         configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
     }
 
     tasks.checkstyleTest {
@@ -39,7 +41,9 @@ allprojects{
 }
 
 dependencies {
+    implementation("io.netty:netty-all:$nettyVersion")
+
+    testImplementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-    compile ("io.netty", "netty-all",  "4.1.5.Final")
 }
