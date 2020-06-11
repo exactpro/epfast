685b149f8af6a226737acea7f91d96e327e18b60
KsovreliAna <ana.ksovreli@exactprosystems.com>
2020-06-11 22:54:41 +0400

    move TemplateTests to com.exactpro.epfast.template.xml and add copyright comment

--- src/test/kotlin/com/exactpro/epfast/template/xml/TemplateTests.kt
+++ src/test/kotlin/com/exactpro/epfast/template/xml/TemplateTests.kt
@@ -1,8 +1,23 @@
-package com.exactpro.epfast.template
+/******************************************************************************
+ * Copyright 2020 Exactpro (Exactpro Systems Limited)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.exactpro.epfast.template.xml
 
 import com.exactpro.epfast.template.assertion.TemplatesComparison.assertTemplateListsAreEqual
 import com.exactpro.epfast.template.dsl.template
-import com.exactpro.epfast.template.xml.XmlTemplates
 import java.io.IOException
 import java.io.InputStream
 import org.junit.jupiter.api.Test
